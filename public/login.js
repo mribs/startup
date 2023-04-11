85f94c4ca26dc29b0e7d58cbd3c41a00f9560f32
mribs <maggieribble@gmail.com>
2023-04-11 12:09:15 -0600

    Service

---
+++ public/login.js
@@ -0,0 +1,24 @@
+function login() {
+    const nameEl = document.querySelector("#name");
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "list.html";
+  }
+
+  function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
